216286cd7fe70bd23e4eb7538d0bfde178239ecb
bukowa <bukowa@users.noreply.github.com>
2022-10-07 12:28:56 +0000

    deploy: 1c213bf9a98fcb2f6c5eaa4e19a3c61d45755b5c

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-Hello World!
+https://bukowa.github.io/digitalocean-images/
+
+[images](./images.json) ||| [sizes](./sizes.json) ||| [regions](./regions.json) |||
+
+images.json.images[].slug
+````yaml
+````
+sizes.json.sizes[].slug
+````yaml
+````
+regions.json.regions[].slug
+````yaml
+````
+images.json.
+````yaml
+{
+  id: unauthorized,
+  message: Unable to authenticate you.
+}
+````
+sizes.json.
+````yaml
+{
+  id: Unauthorized,
+  message: Unable to authenticate you
+}
+````
+regions.json.
+````yaml
+{
+  id: Unauthorized,
+  message: Unable to authenticate you
+}
+````
---
+++ generate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+function get() {
+  curl -s -X GET -H "Content-Type: application/json"  \
+    -H "Authorization: Bearer ${DO_TOKEN}"   \
+    "https://api.digitalocean.com/v2/$1?page=1&per_page=10000" \
+    | jq '.' > "$2"
+}
+
+function simple() {
+  printf "$1$2\n" >> README.md && \
+  printf '````yaml\n' >> README.md && \
+  jq "$2" "$1" | sed 's/\"//g' >> README.md && \
+  printf '````\n' >> README.md
+}
+
+get "images" "images.json"
+get "sizes" "sizes.json"
+get "regions" "regions.json"
+
+rm README.md || true
+printf "https://bukowa.github.io/digitalocean-images/\n\n" >> README.md
+printf "[images](./images.json) ||| " >> README.md
+printf "[sizes](./sizes.json) ||| " >> README.md
+printf "[regions](./regions.json) |||\n\n" >> README.md
+
+simple "images.json" ".images[].slug"
+simple "sizes.json" ".sizes[].slug"
+simple "regions.json" ".regions[].slug"
+
+simple "images.json" "."
+simple "sizes.json" "."
+simple "regions.json" "."
